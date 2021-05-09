faaef1013d5591e4457ae013df98cd359078a1dd
Borislav Pavlov <416657@student.fontys.nl>
2021-05-09 19:02:25 +0300

    added initial socket server

--- src/WebSocket/utils/SocketServer.ts
+++ src/WebSocket/utils/SocketServer.ts
@@ -2,14 +2,17 @@ import ws from 'ws';
 import jwt from 'jsonwebtoken';
 import { Message } from '../messages/Message';
 import Messager from './Messager';
+import OrderAvailableMessage from '../messages/server/OrderAvailableMessage';
+import { ServerMessage } from '../messages/server/ServerMessage';
+import debounce from './debounce';
 
-export class SocketServer<ServerMessage extends Message,ClientMessage extends Message> {
+export class SocketServer {
     private server!: ws.Server;
-    private messager: Messager<ClientMessage["type"],ClientMessage>;
+    private messager: Messager<ServerMessage["type"],ServerMessage>;
     private serverMessagessBuffer: ServerMessage[];
 
     constructor() {
-        this.messager = new Messager<ClientMessage["type"], ClientMessage>();
+        this.messager = new Messager<ServerMessage["type"], ServerMessage>();
         this.serverMessagessBuffer = [];
     }
     
@@ -23,32 +26,45 @@ export class SocketServer<ServerMessage extends Message,ClientMessage extends Me
                 }
             });
         
-        this.server.on("connection", (socket: ws.Server) => {
-            console.log(`App socket open at port ${JSON.stringify(this.server.options.port)}`);
-            this.serverMessagessBuffer.forEach(m => this.send(m));
-            this.serverMessagessBuffer = [];
-            socket.on("message", (e: any) => {
-                const message = JSON.parse(e.data) as ClientMessage;
-                this.messager.publish(message.type, message);
-                console.log(`[Client connected] `);
-            });
+        console.log(`App socket open at port ${JSON.stringify(this.server.options.port)}`);
+        this.serverMessagessBuffer.push(new OrderAvailableMessage({orderNumber:1234}))
+        
+        this.server.on("connection", (_, req) => {
+            console.log(`Client - ${req.headers.origin} connected!`);
+            this.flushServerMessages();
+        });
 
-            socket.on("close", (e: any) => {
-                console.log(
-                    "Socket is closed. Reconnect will be attempted in 2 seconds.",
-                    e.reason
-                );
-            });
-            socket.on("error", (e: any) => {
-                console.error("Socket encountered error: Closing socket");
-            });
+        this.server.on("message", (e: any) => {
+            const message = JSON.parse(e.data) as ServerMessage;
+            this.messager.publish(message.type, message);
+            console.log(`[Client connected] `);
+        });
+
+        this.server.on("close", (e: any) => {
+            console.log(
+                "Socket is closed. Reconnect will be attempted in 2 seconds.",
+                e.reason
+            );
+            this.start();
+        });
+        this.server.on("error", (e: any) => {
+            console.error("Socket encountered error: Closing socket");
+            this.server.close();
         });
     }
 
     public send(message: ServerMessage) {
-        this.server.clients.forEach(client => client.send(message))
+        this.server.clients.forEach(client => {
+            if (client.readyState === client.OPEN) {
+                client.send(JSON.stringify(message))
+            }
+        })
     }
-    
+    private flushServerMessages = debounce(() => {
+                this.serverMessagessBuffer.forEach(m => this.send(m));
+                this.serverMessagessBuffer = [];
+            },1000);
+
     private verifyClientToken(token: string | null) {
         if (token) {
             const decoded = jwt.decode(token) as {[key:string]:any} ;
